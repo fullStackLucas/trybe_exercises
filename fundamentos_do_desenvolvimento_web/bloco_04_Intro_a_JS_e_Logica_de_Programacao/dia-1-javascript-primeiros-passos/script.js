34ba0802ba9d2bbbabdaeb510bbbec7c91b7da65
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-07-28 19:01:47 -0300

    Andiciona exercicio angulos de um triangulo

--- fundamentos_do_desenvolvimento_web/bloco_04_Intro_a_JS_e_Logica_de_Programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos_do_desenvolvimento_web/bloco_04_Intro_a_JS_e_Logica_de_Programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,11 +1,11 @@
-let a = 4;
-let b = 8;
-let c = 5;
+let anguloA = 60;
+let anguloB = 0;
+let anguloC = 60;
 
-if (a > 0) {
-    console.log ("positive");
-} else if (a < 0) {
-    console.log ("negative");
-} else {
-    console.log ("zero");
+if (anguloA <= 0 || anguloB <= 0 || anguloC <= 0) {
+    console.log ("ângulo inválido");
+} else if (anguloA + anguloB + anguloC !== 180) {
+    console.log ("false");
+} else if (anguloA + anguloB + anguloC === 180) {
+    console.log ("true");
 };
