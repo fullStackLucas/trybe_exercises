439782e06b2c7831b72f31b3c912be2a86950771
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-07-28 19:28:39 -0300

    Adiciona exercicio sobre realcao notas e letras

--- fundamentos_do_desenvolvimento_web/bloco_04_Intro_a_JS_e_Logica_de_Programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos_do_desenvolvimento_web/bloco_04_Intro_a_JS_e_Logica_de_Programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,17 +1,19 @@
-let chess = "Batata"
+let score = 10
 
-if (chess.toLowerCase() === "pawn") {
-    console.log ("one step forward");
-} else if (chess.toLowerCase() === "knight") {
-    console.log ("two steps in a straight line, then one step to the side of your choice");
-} else if (chess.toLowerCase() === "bishop") {
-    console.log ("diagonal movements as much as you can");
-} else if (chess.toLowerCase() === "tower") {
-    console.log ("can go forward in a straight line as much as you can");
-} else if (chess.toLowerCase() === "queen") {
-    console.log ("she can move like bishop and tower at the same time");
-} else if (chess.toLowerCase() === "king") {
-    console.log ("can move one step only, for every single spot on the table");
-} else {
-    console.log ("This isn't a chess piece, genius!")
-};
+if (score >= 90) {
+    console.log ("A"); 
+} else if (score >= 80) {
+    console.log ("B");
+} else if (score >= 70) {
+    console.log ("C");
+} else if (score >= 60) {
+    console.log ("D");
+} else if (score >= 50) {
+    console.log ("E")
+} else if (score < 50) {
+    console.log ("F")
+} else if (score > 100 || score < 0) {
+    console.log ("Error, it's an invalid score")
+}
+
+
