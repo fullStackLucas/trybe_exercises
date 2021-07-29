9dded381736b751f8114ed2d5155a380c8534594
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-07-29 18:10:14 -0300

    Finaliza exercicio 5 de 7

--- fundamentos_do_desenvolvimento_web/bloco_04_Intro_a_JS_e_Logica_de_Programacao/dia-2-javascript-array-e-for/script.js
+++ fundamentos_do_desenvolvimento_web/bloco_04_Intro_a_JS_e_Logica_de_Programacao/dia-2-javascript-array-e-for/script.js
@@ -1,5 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0
+/*let sum = 0
 for (let index = 0; index < numbers.length; index += 1) {
   sum += numbers[index];
 };
@@ -7,4 +7,10 @@ if ((sum / numbers.length) > 20) {
     console.log ("valor maior que 20")
 } else {
     console.log ("valor menor ou igual a 20")
-};
+};*/
+higherNumber = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > higherNumber) {
+        higherNumber = numbers[index]
+    };
+}; console.log (higherNumber)
