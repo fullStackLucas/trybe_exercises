6b5d5fc4ccc52a8231a136c1841c2032732717aa
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-07-29 17:52:07 -0300

    Adiciona for/of exe01

--- fundamentos_do_desenvolvimento_web/bloco_04_Intro_a_JS_e_Logica_de_Programacao/dia-2-javascript-array-e-for/script.js
+++ fundamentos_do_desenvolvimento_web/bloco_04_Intro_a_JS_e_Logica_de_Programacao/dia-2-javascript-array-e-for/script.js
@@ -1,5 +1,4 @@
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
-
-for (let index = 0; index < groceryList.length; index += 1) {
-    console.log (groceryList[index]);
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (name of names) {
+    console.log (name);
 };
