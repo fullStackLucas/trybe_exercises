3ef89dcd38626a5d788f6edda172f0d6a72911f6
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-07-29 18:18:14 -0300

    Finaliza exercicio 6 de 7

--- fundamentos_do_desenvolvimento_web/bloco_04_Intro_a_JS_e_Logica_de_Programacao/dia-2-javascript-array-e-for/script.js
+++ fundamentos_do_desenvolvimento_web/bloco_04_Intro_a_JS_e_Logica_de_Programacao/dia-2-javascript-array-e-for/script.js
@@ -7,10 +7,21 @@ if ((sum / numbers.length) > 20) {
     console.log ("valor maior que 20")
 } else {
     console.log ("valor menor ou igual a 20")
-};*/
+};
 higherNumber = 0;
 for (let index = 0; index < numbers.length; index += 1) {
     if (numbers[index] > higherNumber) {
         higherNumber = numbers[index]
     };
-}; console.log (higherNumber)
+}; console.log (higherNumber)*/
+let oddNumbers = 0
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !== 0) {
+        oddNumbers += 1
+    }; 
+}
+if (oddNumbers > 0) {
+    console.log (oddNumbers)
+} else {
+    console.log ("nenhum valor ímpar encontrado")
+}
