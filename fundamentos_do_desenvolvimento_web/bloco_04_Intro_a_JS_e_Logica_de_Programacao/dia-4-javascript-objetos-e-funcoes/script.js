b4cf7037c93c4e73e847581dd55df425a3815f4e
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-02 18:31:33 -0300

    Finaliza exercicio fixaçao 1

---
+++ fundamentos_do_desenvolvimento_web/bloco_04_Intro_a_JS_e_Logica_de_Programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -0,0 +1,9 @@
+// parte I - Objetos For/in
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log ("Bem vinda, " + info.personagem)
