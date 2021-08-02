16030f9fd32f0dbda2d289c1da3bc28c251ee917
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-02 18:36:41 -0300

    Finaliza exercicio 3

--- fundamentos_do_desenvolvimento_web/bloco_04_Intro_a_JS_e_Logica_de_Programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos_do_desenvolvimento_web/bloco_04_Intro_a_JS_e_Logica_de_Programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -1,9 +1,22 @@
 // parte I - Objetos For/in
 
+// 1
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
-  console.log ("Bem vinda, " + info.personagem)
+  //console.log ("Bem vinda, " + info.personagem);
+
+  // 2
+
+  info.recorrente = "Sim";
+
+  // 3
+
+  for (key in info) {
+      console.log(key);
+  };
+
+  
