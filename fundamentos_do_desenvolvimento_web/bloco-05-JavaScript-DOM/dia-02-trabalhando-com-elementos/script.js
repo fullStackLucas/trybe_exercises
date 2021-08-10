a2d529f5d00624d75bdec7879d2d83344169cd51
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-10 15:10:05 -0300

    Finalizo os requisitos 4 e 5

--- fundamentos_do_desenvolvimento_web/bloco-05-JavaScript-DOM/dia-02-trabalhando-com-elementos/script.js
+++ fundamentos_do_desenvolvimento_web/bloco-05-JavaScript-DOM/dia-02-trabalhando-com-elementos/script.js
@@ -3,3 +3,5 @@ let pai = ondeVoceEsta.parentNode;
 pai.style.color = "green";
 let primeiroFilhoDoFilho = document.querySelector("#primeiroFilhoDoFilho");
 primeiroFilhoDoFilho.innerHTML = "Adicione um texto aqui";
+let firstChild = pai.firstChild;
+firstChild = ondeVoceEsta.nextElementSibling;
