0fa285ed6a1944f7f8f9b5478c918a1134eadfee
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-10 16:05:41 -0300

    Completa exercicio 2

--- fundamentos_do_desenvolvimento_web/bloco-05-JavaScript-DOM/dia-02-trabalhando-com-elementos/script.js
+++ fundamentos_do_desenvolvimento_web/bloco-05-JavaScript-DOM/dia-02-trabalhando-com-elementos/script.js
@@ -5,7 +5,8 @@ let primeiroFilhoDoFilho = document.querySelector("#primeiroFilhoDoFilho");
 primeiroFilhoDoFilho.innerHTML = "Adicione um texto aqui";
 let firstChild = pai.firstChild;
 firstChild = ondeVoceEsta.previousElementSibling;
-
+let atencao = ondeVoceEsta.nextSibling;
+atencao.innerText = "Atenção dupla!"
 let thirdChild = ondeVoceEsta.nextElementSibling;
 thirdChild = pai.lastChild.previousElementSibling;
 
@@ -13,3 +14,6 @@ let irmaoOnde = document.createElement("section");
 pai = document.querySelector("#pai");
 pai.appendChild(irmaoOnde);
 irmaoOnde.innerText = "Hello World!"
+let filhoOndeVoceEsta = document.createElement("article");
+ondeVoceEsta.appendChild(filhoOndeVoceEsta);
+filhoOndeVoceEsta.innerText = "I'm back with guns!"
