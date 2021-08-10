0cedb812ec353c59728ab8dc8f989ea4a54a08ed
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-10 15:07:11 -0300

    Finaliza requisito 3 da pratica

---
+++ fundamentos_do_desenvolvimento_web/bloco-05-JavaScript-DOM/dia-02-trabalhando-com-elementos/script.js
@@ -0,0 +1,5 @@
+let ondeVoceEsta = document.querySelector("#elementoOndeVoceEsta");
+let pai = ondeVoceEsta.parentNode;
+pai.style.color = "green";
+let primeiroFilhoDoFilho = document.querySelector("#primeiroFilhoDoFilho");
+primeiroFilhoDoFilho.innerHTML = "Adicione um texto aqui";
