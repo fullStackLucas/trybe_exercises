40f825763cec385933b5cc32a646f33404a33b2a
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-13 17:18:35 -0300

    Finalizo passo 6

--- fundamentos_do_desenvolvimento_web/bloco-05-JavaScript-DOM/dia-02-trabalhando-com-elementos/script.js
+++ fundamentos_do_desenvolvimento_web/bloco-05-JavaScript-DOM/dia-02-trabalhando-com-elementos/script.js
@@ -26,9 +26,12 @@ main.appendChild(sectionLeft);
 sectionLeft.classList.add("left-content");
 
 //6 Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
-
+let sectionRight = document.createElement("section");
+main.appendChild(sectionRight);
+sectionRight.classList.add("right-content");
 
 //7 Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
+
 
 //8 Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um , dois , três , ... como valores da lista. Essa lista deve ser filha do section criado no passo 6;
 
