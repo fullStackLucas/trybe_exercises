98f41b00b4c0a465fd9cc1fdf47bfdcba2d8b218
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-13 17:03:58 -0300

    Finalizo requisito 2

--- fundamentos_do_desenvolvimento_web/bloco-05-JavaScript-DOM/dia-02-trabalhando-com-elementos/script.js
+++ fundamentos_do_desenvolvimento_web/bloco-05-JavaScript-DOM/dia-02-trabalhando-com-elementos/script.js
@@ -1,3 +1,11 @@
+// Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body;
 let h1 = document.createElement("h1");
 document.body.appendChild(h1);
 h1.innerText = "Hello, World!"
+
+// Adicione a tag main com a classe main-content como filho da tag body ;
+let main = document.createElement("main");
+document.body.appendChild(main);
+main.classList.add("main-content");
+
+//
