2636f9b0495f7db568d76ffd73785a472865cf2d
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-13 17:51:22 -0300

    Finalizo requisito 10

--- fundamentos_do_desenvolvimento_web/bloco-05-JavaScript-DOM/dia-02-trabalhando-com-elementos/script.js
+++ fundamentos_do_desenvolvimento_web/bloco-05-JavaScript-DOM/dia-02-trabalhando-com-elementos/script.js
@@ -60,4 +60,17 @@ main.appendChild(segundoH3);
 main.appendChild(terceiroH3);
 
 // Agora que você criou muita coisa, vamos fazer algumas alterações e remoções:
+// 1 Adicione a classe title na tag h1 criada;
+h1.classList.add("title");
+
+// 2 Adicione a classe description nas 3 tags h3 criadas;
+
+
+// 3 Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild();
+
+// 4 Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section;
+
+// 5 Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
+
+// 6 Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
 
