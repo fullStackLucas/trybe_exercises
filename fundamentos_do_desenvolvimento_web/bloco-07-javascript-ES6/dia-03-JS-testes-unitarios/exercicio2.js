c3032400268cf23d3def5e8255ae435e9da63ae4
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-31 20:35:08 -0300

    modifica questao 2.3

--- fundamentos_do_desenvolvimento_web/bloco-07-javascript-ES6/dia-03-JS-testes-unitarios/exercicio2.js
+++ fundamentos_do_desenvolvimento_web/bloco-07-javascript-ES6/dia-03-JS-testes-unitarios/exercicio2.js
@@ -25,7 +25,8 @@ assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
 // Verifique se o array passado por parâmetro não sofreu alterações
 
 const newArray = [3, 4, 5, 6, 7];
-assert.deepStrictEqual(myRemove(newArray, 8), [3, 4, 5, 6, 7]);
+myRemove(newArray, 3)
+assert.deepStrictEqual(newArray, [3, 4, 5, 6, 7]);
 
 // Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
 
