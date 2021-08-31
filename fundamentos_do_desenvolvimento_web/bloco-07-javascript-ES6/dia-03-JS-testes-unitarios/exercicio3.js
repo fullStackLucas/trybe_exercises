03c1cbbaf5fddc8f1f8a445580b6d8c5778c7361
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-31 20:55:23 -0300

    3.3 Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações

--- fundamentos_do_desenvolvimento_web/bloco-07-javascript-ES6/dia-03-JS-testes-unitarios/exercicio3.js
+++ fundamentos_do_desenvolvimento_web/bloco-07-javascript-ES6/dia-03-JS-testes-unitarios/exercicio3.js
@@ -18,4 +18,14 @@ function myRemoveWithoutCopy(arr, item) {
 
 assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
 
-// 
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+// Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
+
+const newArray = [3, 4, 5, 6, 7];
+myRemoveWithoutCopy(newArray, 3)
+assert.notDeepStrictEqual(newArray, [3, 4, 5, 6, 7]);
+
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
