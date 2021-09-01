c3efba97d2d489ed77a5b4c23d50401d61696327
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-31 21:07:47 -0300

    4.1 Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado

---
+++ fundamentos_do_desenvolvimento_web/bloco-07-javascript-ES6/dia-03-JS-testes-unitarios/exercicio4.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+// implemente seus testes aqui
+
+// Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
+
+assert.strictEqual(myFizzBuzz(30), 'fizzbuzz');
+
+// Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
