0bd6f12e55a87337cdde2f46cc09d0ba94d3f108
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-31 21:26:34 -0300

    5.1 verifica se o obj1 é igual ao obj2

---
+++ fundamentos_do_desenvolvimento_web/bloco-07-javascript-ES6/dia-03-JS-testes-unitarios/exercicio5.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+
+const obj1 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  description: 'My Description',
+  title: 'My Title',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
+
+// implemente seus testes aqui
+
+// verifica se o obj1 é igual ao obj2
+
+assert.deepStrictEqual(obj1, obj2);
