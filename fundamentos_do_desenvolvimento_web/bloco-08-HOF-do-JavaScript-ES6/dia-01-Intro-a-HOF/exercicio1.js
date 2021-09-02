c0808ac2e336b303b6fadb2c04e6def13d43c96f
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-09-02 18:59:05 -0300

    Cria funçao de checagem de gabarito

--- fundamentos_do_desenvolvimento_web/bloco-08-HOF-do-JavaScript-ES6/dia-01-Intro-a-HOF/exercicio1.js
+++ fundamentos_do_desenvolvimento_web/bloco-08-HOF-do-JavaScript-ES6/dia-01-Intro-a-HOF/exercicio1.js
@@ -15,4 +15,53 @@ const newInput = (name) => {
   }
 }
 
-console.table(newEmployees(newInput));
+// 2 Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const drawResult = (number, callback) => {
+  const hofNumber = Math.ceil(Math.random() * 5);
+  if (callback(number, hofNumber) === true) {
+    return `O número sorteado foi ${hofNumber}. Parabéns, você ganhou!!`
+  } else {
+    return `O número sorteado foi ${hofNumber}. Tente Novamente`
+  }
+};
+
+const checkNumber = (number, drawNumber) => {
+  if (number === drawNumber) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// console.log(drawResult(5, checkNumber));
+
+//  Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas. 
+
+//Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+
+
+const finalScore = (answersSheet, studentAnswers, check) => {
+  return `Sua nota final foi: ${check(answersSheet, studentAnswers)}`
+};
+
+const check = (answersSheet, studantAnswers) => {
+  let score = 0
+  for (let index = 0; index < answersSheet.length; index +=1){
+    if (answersSheet[index] === studantAnswers[index]) {
+      score += 1;
+    } else if(studantAnswers[index] === 'N.A') {
+      score = score;
+    } else {
+      score -= 0.5;
+    }
+  }
+  return score;
+};
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+// console.log(finalScore(RIGHT_ANSWERS, STUDENT_ANSWERS, check));
+
+
