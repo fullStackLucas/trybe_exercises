3814c432e207584cf883a461684dd75f1559cc28
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-09-03 16:07:37 -0300

    finaliza terceiro exercicio do find

--- fundamentos_do_desenvolvimento_web/bloco-08-HOF-do-JavaScript-ES6/dia-02-JS-HOF-forEach-find-some-every-sort/fixacao8-2.js
+++ fundamentos_do_desenvolvimento_web/bloco-08-HOF-do-JavaScript-ES6/dia-02-JS-HOF-forEach-find-some-every-sort/fixacao8-2.js
@@ -33,3 +33,16 @@ const search = names.find(findNameWithFiveLetters);
 
 console.log(search);
 
+// find 3:  musica com id exato 31031685
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+const findMusic = (musica) => musica.id === '31031685';
+
+const searchId = musicas.find(findMusic);
+
+console.log(searchId);
