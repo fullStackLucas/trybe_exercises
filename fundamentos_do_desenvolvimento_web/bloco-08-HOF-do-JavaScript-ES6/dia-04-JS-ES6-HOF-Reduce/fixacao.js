12f481b9668a699cc2717107cbf9d8a652dd8a6e
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-09-09 19:18:33 -0300

    finaliza exercicio 1

---
+++ fundamentos_do_desenvolvimento_web/bloco-08-HOF-do-JavaScript-ES6/dia-04-JS-ES6-HOF-Reduce/fixacao.js
@@ -0,0 +1,108 @@
+/* const numbers = [2, 4, 28, 36, 15, 27, 12];
+const reducer = (result, number) => ((result > number) ? result : number);
+
+const biggerNumber = numbers.reduce(reducer, 0);
+
+console.log(biggerNumber); */
+
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+/* const reducer = (result, number) => {
+  if (number % 2 === 0) {
+    result += number;
+  }
+  
+  return result;
+}
+
+const evenSum = numbers.reduce(reducer, 0);
+console.log(evenSum); */
+
+
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 },
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
+
+// Agora crie uma função usando os dados dos estudantes que usamos no conteúdo do dia anterior, para mostrar na tela um relatório que diz em qual matéria o estudante foi melhor. Você usará tanto o map quanto, dentro dele, o reduce !
+
+const newStudents = estudantes.map ((estudante) => {
+  const nota = estudante.materias.reduce((nota, current) => (nota.nota > current.nota) ? nota : current)
+  const newArray = {
+    fullName: `${estudante.nome} ${estudante.sobrenome}`,
+    betterSubject: `${nota.name}`
+  }
+  return newArray;
+})
+
+console.log(newStudents);
